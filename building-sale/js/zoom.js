7da21087f63d146f3b6591342553f1e4bb293b2d
tatafox <pavlovetstatyana@gmail.com>
2021-06-06 19:43:00 +0300

    refractor: clear code

--- building-sale/js/zoom.js
+++ building-sale/js/zoom.js
@@ -10,7 +10,6 @@ document
 function zoomElements(e) {
   console.log(window.event, e.srcElement);
   const element = e.srcElement;
-  //const newElement = document.createElement("div");
   const childrenDiv = document.createElement("div");
   zoomIn.appendChild(childrenDiv);
   childrenDiv.innerHTML = element.outerHTML;
@@ -19,21 +18,10 @@ function zoomElements(e) {
     ? zoomIn.children[0].children[0].classList.add("zoom--width")
     : zoomIn.children[0].children[0].classList.add("zoom--height");
   document.body.classList.add("body--lock");
-  //newElement.addEventListener("click", deleteElement); //() => (newElement.innerHTML = ""));
-  //document.body.appendChild(newElement);
-  //element.classList.toggle("zoom--active");
-  //element.classList.toggle("zoom--height");
 }
 
 function deleteElement(e) {
-  console.log(e, e.path[2] instanceof HTMLBodyElement, e.srcElement);
   zoomIn.innerHTML = "";
   zoomIn.classList.remove("zoom--active");
   document.body.classList.remove("body--lock");
-  /*e.path[2] instanceof HTMLBodyElement
-    ? e.path[1].removeEventListener("click", deleteElement)
-    : e.path[2].removeEventListener("click", deleteElement);
-  e.path[2] instanceof HTMLBodyElement
-    ? (e.path[1].innerHTML = "")
-    : (e.path[2].innerHTML = "");*/
 }
